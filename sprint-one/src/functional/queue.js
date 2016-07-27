4db9fadc4131deda06e2909b4ae947858bf31a78
MikeBordelon <374mike@gmail.com>
2016-07-27 15:43:39 -0700

    Implement queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var count = 0;
+  var firstElement = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
+    console.log(storage);
   };
 
   someInstance.dequeue = function() {
+    var result = storage[firstElement];
+    delete storage[firstElement];
+    console.log(storage);
+
+    if (count > 0) {
+      count--;
+    } 
+    if(count>1){firstElement++; }
+    console.log(firstElement);
+    return result;  
+
+
   };
 
   someInstance.size = function() {
+
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size ++;
   };
 
   someInstance.pop = function() {
+    //take the last value we pushed out of storage and return it
+    //the last value is the one with the highest numerical key
+    var result = storage[size-1];
+    delete storage[size-1];
+    if(size>0){size--;}
+    return result;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
