8866731f332e9dc6be3f00951629796249f455c4
MikeBordelon <374mike@gmail.com>
2016-07-28 19:15:24 -0700

    Implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
-var setPrototype = {};
+var setPrototype = {
+
+};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] === item) {
+    return true;
+  } else {
+   return   false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  
+  delete this._storage[item];
+  
 };
 
 /*
